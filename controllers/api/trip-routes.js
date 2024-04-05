5c7d01b6b8d285c5f968cb58187cfc5422973ae3
sarahd <sarahjdavila210@gmail.com>
2024-04-04 19:10:36 -0700

    updated trip-routes.js

---
+++ controllers/api/trip-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { Trip } = require('../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const { location, travel_dates, user_id } = req.body;
+
+        const trip = await Trip.create({
+            location,
+            travel_dates,
+            user_id
+        });
+
+        res.status(200).json(trip);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+module.exports = router;
